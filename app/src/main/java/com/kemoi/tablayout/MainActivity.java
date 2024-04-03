15b55f01bb86f9be5a32c9782cf9829927b77c1e
Kemoi <aaronkipkirui015@gmail.com>
2024-04-03 13:37:25 +0300

    layouts and MainActivity commits

--- app/src/main/java/com/kemoi/tablayout/MainActivity.java
+++ app/src/main/java/com/kemoi/tablayout/MainActivity.java
@@ -1,8 +1,13 @@
 package com.kemoi.tablayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//      Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(getResources().getString(R.string.app_name));
+//      TabLayout
+        TabLayout tabLayout = findViewById(R.id.tablayout);
+//        TabItems
+        TabItem tabChats = findViewById(R.id.tabChats);
+        TabItem tabStatus = findViewById(R.id.tabStatus);
+        TabItem tabCalls = findViewById(R.id.tabCalls);
+//      ViewPager
+        ViewPager viewPager = findViewById(R.id.viewPager);
     }
 }
